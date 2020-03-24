464ce97d64f6b914515201ba4b5c46cf97f6a582
Cc <85765066@qq.com>
2020-03-24 11:56:14 +0800

    .

--- audiovideo/src/main/java/com/eju/cy/audiovideo/im/entrance/EjuImSdkCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.eju.cy.audiovideo.im.entrance;
-
-public interface EjuImSdkCallBack {
-
-    void onSuccess(Object data);
-
-    void onError(String module, int errCode, String errMsg);
-}
